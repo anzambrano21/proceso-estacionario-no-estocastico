1f1f62bbb5430b3d9f07f36260731e08c91c7586
anzambrano21 <lolepico1234@gmail.com>
2024-06-03 19:33:21 -0400

    definir estructura de la ventana

--- no estocastico.py
+++ no estocastico.py
@@ -1,6 +1,7 @@
-
-
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 class divisasEURUS:
     def __init__(self):
@@ -11,6 +12,13 @@ class divisasEURUS:
         self.coti["cierre"]=cier
     def setFecha(self,fech):
         self.coti["fech"]=fech
+    def model(self,p,d,q):
+        order = (p, d, q)  # Orden (p, d, q)
+        model = sm.tsa.ARIMA(self.df['Diff'].dropna(), order=order)
+        self.results = model.fit()
+    def prediccion(self):
+        forecast_steps = 10
+        forecast, stderr, conf_int = self.results.forecast(steps=forecast_steps)
     
 
 
--- pagina.py
+++ pagina.py
@@ -1,12 +1,51 @@
 import flet
+from flet import Column,Row,Container,margin
 class pagina:
     def __init__(self,page:flet.Page):
         self.page=page
-        self.page.window_height=500
-        self.page.window_width=500
+        self.page.window_height=1000
+        self.page.window_width=1000
         #caselar el cambio de tamaño
         self.page.window_resizable=False
         self.page.title="no estacionario"
-        self.page.vertical_alignment = flet.MainAxisAlignment.CENTER
+        
+        self.fecha=flet.TextField(label="Fecha")
+       
+        self.componentes()
         self.page.update()
+        
+    def componentes(self):
+        self.page.add(Column([
+            flet.Text("Proceso Estocástico no Estacionario en Divisas Euro/US", text_align=flet.TextAlign.CENTER)]))
+        
+        # Crear un contenedor y agregar los controles dentro de él
+        self.contenedor1 = Container(content=Column([Row([self.fecha,
+                                                     flet.TextField(label="Cierre"),flet.ElevatedButton(text="Agragar")]),flet.Text(value="model"),
+                                                     Row([flet.TextField(label="P"),flet.TextField(label="D"),flet.TextField(label="Q")]),
+                                                     flet.Container(content=Column([Row([
+                                                                                flet.DataTable(columns=[
+                                                                                    flet.DataColumn(flet.Text("Fecha")),
+                                                                                    flet.DataColumn(flet.Text("Cierre"))
+                                                                                ],
+                                                                                rows=[]
+                                                                                )
+                                                                            ])
+                                                         
+                                                                        ]),
+                                                                        margin=margin.only(0,25),
+                                                                        padding=10,
+                                                                        alignment=flet.alignment.center,
+                                                                        width=950,
+                                                                        height=450,
+                                                                        border_radius=10,
+                ),]),                   
+                    margin=10,
+                    width=920,
+                    height=450,
+                    )
+        self.contenedor1.margin = margin.all(20)
+
+        self.page.add(Row([self.contenedor1]))
+
+
 
