7fb717677be186022f2c197c99d7a14dfd2af525
anzambrano21 <lolepico1234@gmail.com>
2024-06-03 11:08:40 -0400

    generar set a los atributos de la calse 1

---
+++ no estocastico.py
@@ -0,0 +1,12 @@
+class divisasEURUS:
+    def __init__(self):
+        self.coti["dif"]
+        self.coti["cierre"]
+        self.coti["fecha"]
+    def setCierre(self,cier):
+        self.coti["cierre"]=cier
+    def setFecha(self,fech):
+        self.coti["fech"]=fech
+    
+
+
